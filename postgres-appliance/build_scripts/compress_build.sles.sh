4ad7d8d53b908c14924fa671c25d28371dd24d57
Haris L <haris.lihovac@cybertec.at>
2023-04-25 08:44:36 +0000

    fix busybox

--- postgres-appliance/build_scripts/compress_build.sles.sh
+++ postgres-appliance/build_scripts/compress_build.sles.sh
@@ -2,31 +2,40 @@
 
 set -ex
 
-zypper update -y
-zypper -n install -y busybox busybox-xz
+zypper -q update -y
+zypper -n install busybox xz tar awk
 zypper -q clean -a
 
 #rm -rf /var/lib/apt/lists/* /var/cache/debconf/* /usr/share/doc /usr/share/man /etc/rc?.d /etc/systemd
-ln -snf busybox /bin/sh
+rm -rf /var/cache/zypp/ /usr/share/doc /usr/share/man /etc/systemd
+ln -snf /usr/bin/busybox /bin/sh
 
 files="/bin/sh"
 arch=$(uname -m)
 darch=$(uname -m | sed 's/_/-/')
 
-IFS=" " read -r -a libs <<< "$(ldd $files | awk '{print $3;}' | grep '^/' | sort -u)"
-libs+=(/lib/ld-linux-"$darch".so.* \
-    /lib/"$arch"-linux-gnu/ld-linux-"$darch".so.* \
-    /lib/"$arch"-linux-gnu/libnsl.so.* \
-    /lib/"$arch"-linux-gnu/libnss_compat.so.*)
+#IFS=" " read -r -a libs <<< "$(ldd $files | awk '{print $3;}' | grep '^/' | sort -u | tr -d '\n')"
+IFS=" " read -r -a libs <<< "$(ldd $files | awk '{print $3;}' | sort -u | tr '\n' ' ')"
+libs+=(
+    /lib64/ld-linux-"$darch".so.* \
+    /lib64/libnsl.so.* \
+    /lib64/libnss_compat.so.* \
+    /usr/lib64/libevent*.so.* \
+    /usr/lib64/libbrotlienc.so.*)
 
 (echo /var/run /var/spool "$files" "${libs[@]}" | tr ' ' '\n' && realpath "$files" "${libs[@]}") | sort -u | sed 's/^\///' > /exclude
 
 find /etc/alternatives -xtype l -delete
-save_dirs=(usr lib var bin sbin etc/ssl etc/init.d etc/alternatives etc/apt)
+save_dirs=(usr lib var bin sbin etc/ssl etc/init.d etc/alternatives)
 XZ_OPT=-e9v tar -X /exclude -cpJf a.tar.xz "${save_dirs[@]}"
 
-rm -fr /usr/local/lib/python*
+rm -fr /usr/lib/python*
 
-/bin/busybox sh -c "(find ${save_dirs[*]} -not -type d && cat /exclude /exclude && echo exclude) | sort | uniq -u | xargs /bin/busybox rm"
-/bin/busybox --install -s
-/bin/busybox sh -c "find ${save_dirs[*]} -type d -depth -exec rmdir -p {}; 2> /dev/null"
+#/bin/busybox sh -c "(find ${save_dirs[*]} -not -type d && cat /exclude /exclude && echo exclude) | sort | uniq -u | xargs /bin/busybox rm"
+/usr/bin/busybox sh -c "(find ${save_dirs[*]} -not -type d && cat /exclude /exclude && echo exclude) | sort | uniq -u | xargs /usr/bin/busybox rm -f"
+
+#/bin/busybox --install -s
+busybox --list | while read applet; do busybox ln -sf /usr/bin/busybox /bin/$applet; done
+
+#/bin/busybox sh -c "find ${save_dirs[*]} -type d -depth -exec rmdir -p {}; 2> /dev/null"
+/usr/bin/busybox sh -c "find ${save_dirs[*]} -type d -depth -exec rmdir -p {} \; 2> /dev/null"
